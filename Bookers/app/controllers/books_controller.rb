538acdd33b4693e643c9e165fd724b17ab8fc6ba
志村隼斗 <daken@shimurahayatonoMacBook-Air.local>
2019-05-12 23:43:49 +0900

    [add]index追加

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,13 +1,30 @@
 class BooksController < ApplicationController
   def show
+  	@book = Book.find(params[:id])
   end
 
   def index
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def top
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
 end
