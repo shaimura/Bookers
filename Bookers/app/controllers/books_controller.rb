3f3a93c747a56eabd9160d51153b91a0851e1ccf
志村隼斗 <daken@shimurahayatonoMacBook-Air.local>
2019-05-13 17:23:32 +0900

    [update]レイアウト変更

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -12,7 +12,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id), notice: "Book was successfully created."
   end
 
   def top
@@ -25,13 +25,13 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book)
+    redirect_to book_path(book), notice: "Book was successfully created."
   end
 
   def destroy
   	book = Book.find(params[:id])
   	book.destroy
-  	redirect_to books_path
+  	redirect_to books_path, notice: "Book was successfully destroyed."
   end
 
   private
